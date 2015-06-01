ef8459b7c8e67f6a087c10ca9c24f2e4f0e9f8c4
Chris Butler <chris@platformthirteen.com>
2015-06-01 14:46:49 -0400

    tweak styles and include css

--- package.js
+++ package.js
@@ -1,19 +1,15 @@
 Package.describe({
   name: 'chriswessels:slideout',
   version: '0.1.9',
-  // Brief, one-line summary of the package.
   summary: 'A touch slideout navigation menu for your mobile web apps',
-  // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/chriswessels/meteor-slideout',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  /* Add Slideout.js */
   api.addFiles('lib/dist/slideout.min.js', 'client');
+  api.addFiles('lib/index.css', 'client');
   api.addFiles('export.js', 'client');
   api.export('Slideout');
 });
